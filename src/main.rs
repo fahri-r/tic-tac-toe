033b60e009d8dadc0d4d8bcffef6c4059de2cedd
fahri-r <00.fahri.r@gmail.com>
2025-03-08 11:55:08 +0700

    add winning condition

--- src/main.rs
+++ src/main.rs
@@ -58,6 +58,29 @@ fn is_over(squares: &mut [char; 9]) -> bool {
     squares.iter().all(|&square| square == 'X' || square == 'O')
 }
 
+fn is_win(squares: &mut [char; 9]) -> bool {
+    for i in 0..3 {
+        //Check vertical
+        if squares[i] == squares[i + 3] && squares[i] == squares[i + 6] {
+            return true;
+        }
+
+        //Check horizontal
+        let i = i * 3;
+        if squares[i] == squares[i + 1] && squares[i] == squares[i + 2] {
+            return true;
+        }
+    }
+
+    //Check diagonal
+    if (squares[0] == squares[4] && squares[0] == squares[8]) || (squares[2] == squares[4] && squares[2] == squares[6])
+    {
+        return true;
+    }
+
+    return false;
+}
+
 fn main() {
     let mut squares = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
     let mut player = 'X';
@@ -67,6 +90,12 @@ fn main() {
     loop {
         draw_board(&mut squares);
         ask_player(&mut squares, player);
+
+        if is_win(&mut squares) {
+            draw_board(&mut squares);
+            println!("{} Win!", player);
+            break;
+        }
 
         if is_over(&mut squares) {
             draw_board(&mut squares);
